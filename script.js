7dae37af7a321cf4920af9781c9e46a533ab8fcc
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-03-28 22:06:33 +0100

    fade efx added

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function animate() {
 
 animate();
 
-
+//will implement this later
 
 /* const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
