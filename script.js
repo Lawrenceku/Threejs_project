f2cbfb71c98cea9b1fc31331bcba0861abf417a7
Lawrence Eniola <lawrencekunmilola@gmail.com>
2023-03-28 20:49:55 +0100

    3d rotating cube thingy

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scene = new THREE.Scene();
+/* const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById('myCanvas') });
       renderer.setSize(window.innerWidth, window.innerHeight);
@@ -17,6 +17,28 @@ const scene = new THREE.Scene();
 
       function animate() {
         requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      }
+
+      animate(); */
+
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById('myCanvas') });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
         renderer.render(scene, camera);
       }
 
